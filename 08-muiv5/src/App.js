d9646734ec86212b0e808a24c6f8bbcc92db4235
SOunit <workbookwork7@gmail.com>
2022-06-03 14:55:28 -0700

    add sample

--- 08-muiv5/src/App.js
+++ 08-muiv5/src/App.js
@@ -1,19 +1,31 @@
 import "./App.css";
 import TourCard from "./components/TourCard";
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import SearchAppBar from "./components/AppBar";
+import cities from "./data.json";
 
 function App() {
   return (
     <div className="App">
       <SearchAppBar />
-      <Container>
-        <Grid container spacing={5}>
-          <TourCard />
-          <TourCard />
-          <TourCard />
-          <TourCard />
-        </Grid>
+      <Container sx={{ marginY: 5 }}>
+        {cities.map((city) => (
+          <>
+            <Typography
+              variant="h4"
+              component="h2"
+              marginTop={5}
+              marginBottom={3}
+            >
+              Top {city.name} Tour
+            </Typography>
+            <Grid container spacing={5}>
+              {city.tours.map((tour, index) => (
+                <TourCard tour={tour} key={index} />
+              ))}
+            </Grid>
+          </>
+        ))}
       </Container>
     </div>
   );
--- 08-muiv5/src/components/TourCard.js
+++ 08-muiv5/src/components/TourCard.js
@@ -29,38 +29,39 @@ const theme = createTheme({
   },
 });
 
-const TourCard = () => {
+const TourCard = ({ tour }) => {
   return (
     <Grid item xs={3}>
       <ThemeProvider theme={theme}>
         <Paper elevation={3}>
-          <img
-            src="https://source.unsplash.com/random"
-            alt=""
-            className="img"
-          />
+          <img src={tour.image} alt="" className="img" />
           <Box paddingX={1}>
             <Typography variant="subtitle1" component="h2">
-              Immerse into the fall
+              {tour.name}
             </Typography>
             <Box sx={{ display: "flex", alignItems: "center" }}>
               <AccessTime sx={{ width: 12.5 }} />
               <Typography variant="body2" component="p" marginLeft={0.5}>
-                5 hours
+                {tour.duration} hours
               </Typography>
             </Box>
             <Box sx={{ display: "flex", alignItems: "center", marginTop: 3 }}>
-              <Rating value={4.5} precision={0.5} readOnly size="small" />
+              <Rating
+                value={tour.rating}
+                precision={0.5}
+                readOnly
+                size="small"
+              />
               <Typography variant="body2" component="p" marginLeft={0.5}>
-                4.5
+                {tour.rating}
               </Typography>
               <Typography variant="body2" component="p" marginLeft={1.5}>
-                (655 reviews)
+                ({tour.numberOfReviews} reviews)
               </Typography>
             </Box>
             <Box>
               <Typography variant="h6" component="h3" marginTop={0}>
-                From C $147
+                From C ${tour.price}
               </Typography>
             </Box>
           </Box>
