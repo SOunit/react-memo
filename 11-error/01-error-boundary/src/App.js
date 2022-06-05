db1d9ff308b53ff7533416a412fb2ea280ecdcab
SOunit <workbookwork7@gmail.com>
2022-06-05 13:41:02 -0700

    add sample

---
+++ 11-error/01-error-boundary/src/App.js
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Hero heroName="Batman" />
+      <Hero heroName="Superman" />
+    </div>
+  );
+}
+
+export default App;
