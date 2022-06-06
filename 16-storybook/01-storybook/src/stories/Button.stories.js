ded6c91815f521e5fd6b539ac439643d62a15a78
SOunit <workbookwork7@gmail.com>
2022-06-06 14:10:41 -0700

    add sample

--- 16-storybook/01-storybook/src/stories/Button.stories.js
+++ 16-storybook/01-storybook/src/stories/Button.stories.js
@@ -3,6 +3,29 @@ import Button from "../components/Button";
 export default {
   title: "Button",
   component: Button,
+  argTypes: { handleClick: { action: "handleClick" } },
 };
 
-export const Red = () => <Button label="Press Me" backgroundColor="red" />;
+const Template = (args) => <Button {...args} />;
+
+// export const Red = () => <Button label="Press Me" backgroundColor="red" />;
+export const Red = Template.bind({});
+Red.args = {
+  backgroundColor: "red",
+  label: "Press Me",
+  size: "md",
+};
+
+export const Green = Template.bind({});
+Green.args = {
+  backgroundColor: "green",
+  label: "Press Me",
+  size: "md",
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  backgroundColor: "green",
+  label: "Press Me",
+  size: "sm",
+};
