88465c657c52941fe2f278cee12d297f4646c558
SOunit <workbookwork7@gmail.com>
2022-06-22 13:17:49 -0700

    add sample

--- 17-component-samples/src/App.js
+++ 17-component-samples/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import RadioButtonWithForm from "./pages/RadioButtonWithForm";
+import ReusableForm from "./pages/ReusableForm";
 
 function App() {
   return (
     <div className="App">
       <RadioButtonWithForm />
+      <ReusableForm />
     </div>
   );
 }
---
+++ 17-component-samples/src/pages/ReusableForm.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ReusableForm = () => {
+  return (
+    <div>
+      <h1>ReusableForm</h1>
+      <p>FIXME: replace url with sample</p>
+      <h2>local url</h2>
+      <p>
+        05-ui-framework-samples/05-muiv5-samples-js/src/components/form/EmployeeForm.jsx
+      </p>
+      <h2>github url</h2>
+      <a href="https://github.com/SOunit/react_memo/tree/main/05-ui-framework-samples/05-muiv5-samples-js/src/components/form">
+        github repo url
+      </a>
+    </div>
+  );
+};
+
+export default ReusableForm;
