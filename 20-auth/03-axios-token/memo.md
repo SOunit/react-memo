1cdeb0972eb164dac2659213606e3639f5796e77
SOunit <workbookwork7@gmail.com>
2022-06-23 16:20:56 -0700

    add sample

---
+++ 20-auth/03-axios-token/memo.md
@@ -0,0 +1,10 @@
+# url
+
+- ReactSecurity - Attach a JSON Web Token in an Axios Request
+  https://www.youtube.com/watch?v=-u04JD5Eo-c
+
+# points
+
+- 2 approaches to set auth-token to axios
+  - general setup
+  - create unique axios
--- 20-auth/03-axios-token/src/App.js
+++ 20-auth/03-axios-token/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+const accessToken = "accessTokenFromJWT";
+const apiUrl = "https://jsonplaceholder.typicode.com";
+
+// pattern1. general setup
+axios.interceptors.request.use(
+  (config) => {
+    config.headers.authorization = `Bearer ${accessToken}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// pattern2. setup specific axios
+const authAxios = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+  },
+});
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [requestError, setRequestError] = useState();
+
+  const fetchData = async () => {
+    console.log("click");
+
+    try {
+      // pattern1.
+      // const response = await axios.get(`${apiUrl}/users`);
+
+      // pattern2. create unique axios
+      const response = await authAxios.get(`/users`);
+      setUsers(response.data);
+    } catch (error) {
+      setRequestError(error.message);
+    }
+  };
+
+  const clickHandler = () => {
+    fetchData();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={clickHandler}>Request!!!</button>
+      {requestError && <p>{requestError}</p>}
+      {users.map((user) => {
+        return <div key={user.id}>{user.name}</div>;
+      })}
     </div>
   );
 }
