6ce00dfce279bf1d5c1f76c16c7cc8921e089fdc
SOunit <workbookwork7@gmail.com>
2022-06-25 11:13:27 -0700

    add navigation

--- 20-auth/04-jwt-react-node/client/src/App.js
+++ 20-auth/04-jwt-react-node/client/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route to="/signup" element={<Signup />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ 20-auth/04-jwt-react-node/client/src/components/Navigation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navigation = () => {
+  return <div className="text-3xl font-bold underline">Navigation</div>;
+};
+
+export default Navigation;
