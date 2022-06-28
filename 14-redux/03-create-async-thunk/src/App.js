c67f893057ec9e76c1355520d79841b4648c7f4e
SOunit <workbookwork7@gmail.com>
2022-06-28 16:40:54 -0700

    add sample

--- 14-redux/03-create-async-thunk/src/App.js
+++ 14-redux/03-create-async-thunk/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import { getUsers } from "./features/users/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const { users } = useSelector((state) => state.users);
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Redux toolkit createAsyncThunk</h1>
+      {users && users.map((user) => <div key={user.id}>{user.name}</div>)}
     </div>
   );
 }
