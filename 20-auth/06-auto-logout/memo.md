9ebef1531a4cfe57f926a74a546001de782a223a
SOunit <workbookwork7@gmail.com>
2022-06-28 10:31:11 -0700

    add sample

---
+++ 20-auth/06-auto-logout/memo.md
@@ -0,0 +1,20 @@
+# login
+
+- create expiration date
+- save data
+  - token
+  - expiration date
+
+# logout
+
+- delete data
+  - token
+  - expiration date
+
+# auto-login
+
+- login if expiration date is future than current date
+
+# auto-logout
+
+- logout if check expiration date if past than current date
--- 20-auth/06-auto-logout/src/App.js
+++ 20-auth/06-auto-logout/src/App.js
@@ -1,26 +1,59 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import { ONE_SECOND } from "./constants/time";
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
 
-  const loginHandler = () => {
-    localStorage.setItem("test-token", JSON.stringify({ token: "test token" }));
+  const logout = () => {
+    localStorage.removeItem("test-token");
+    setIsLogin(false);
+  };
+
+  const login = (expirationDate) => {
+    const currentTime = new Date().getTime();
+    const tokenExpirationDate =
+      expirationDate || new Date(currentTime + ONE_SECOND);
+
+    console.log("tokenExpirationDate", tokenExpirationDate);
+
+    localStorage.setItem(
+      "test-token",
+      JSON.stringify({
+        token: "test token",
+        expiration: tokenExpirationDate.toISOString(),
+      })
+    );
     setIsLogin(true);
   };
 
-  useEffect(() => {
-    const token = localStorage.getItem("test-token");
+  const loginHandler = () => {
+    login();
+  };
 
-    if (token) {
+  const logoutHandler = () => {
+    logout();
+  };
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("test-token"));
+
+    console.log("storedData", storedData);
+
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
       setIsLogin(true);
     }
   }, []);
 
   return (
     <div className="App">
-      <h1>Login? {String(isLogin)}</h1>
+      <h1>isLogin: {String(isLogin)}</h1>
       <button onClick={loginHandler}>Login</button>
+      <button onClick={logoutHandler}>Logout</button>
     </div>
   );
 }
