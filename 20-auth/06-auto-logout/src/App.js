dbaaf5b605b81108e3876c39cfcdf73a3f67d278
SOunit <workbookwork7@gmail.com>
2022-06-28 11:19:58 -0700

    add sample

--- 20-auth/06-auto-logout/src/App.js
+++ 20-auth/06-auto-logout/src/App.js
@@ -1,31 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import { ONE_SECOND } from "./constants/time";
+import useAuth from "./hooks/auth-hook";
 
 function App() {
-  const [isLogin, setIsLogin] = useState(false);
-
-  const logout = () => {
-    localStorage.removeItem("test-token");
-    setIsLogin(false);
-  };
-
-  const login = (expirationDate) => {
-    const currentTime = new Date().getTime();
-    const tokenExpirationDate =
-      expirationDate || new Date(currentTime + ONE_SECOND);
-
-    console.log("tokenExpirationDate", tokenExpirationDate);
-
-    localStorage.setItem(
-      "test-token",
-      JSON.stringify({
-        token: "test token",
-        expiration: tokenExpirationDate.toISOString(),
-      })
-    );
-    setIsLogin(true);
-  };
+  const { isLogin, login, logout } = useAuth();
 
   const loginHandler = () => {
     login();
@@ -34,20 +12,6 @@ function App() {
   const logoutHandler = () => {
     logout();
   };
-
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem("test-token"));
-
-    console.log("storedData", storedData);
-
-    if (
-      storedData &&
-      storedData.token &&
-      new Date(storedData.expiration) > new Date()
-    ) {
-      setIsLogin(true);
-    }
-  }, []);
 
   return (
     <div className="App">
