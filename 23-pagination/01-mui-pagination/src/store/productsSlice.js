e5072d8dea7aab20bd0cb5a8709dd49aa9f5319f
SOunit <workbookwork7@gmail.com>
2022-07-14 16:32:34 -0700

    add sample

---
+++ 23-pagination/01-mui-pagination/src/store/productsSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [],
+};
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {
+    setProducts: (state, action) => {
+      state.products = action.payload;
+    },
+  },
+});
+
+export const productsActions = productsSlice.actions;
+export const productsReducer = productsSlice.reducer;
